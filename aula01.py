c4c6820b8cb13b38e86f16e89d686cf1948aee69
iamluissgustavo <estudanteluis2007@gmail.com>
2024-08-15 08:00:35 -0300

    Create aula01.py

---
+++ aula01.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self):
+        self.nome = input('Qual seu nome? ')
+        self.cpf = input('Qual seu CPF? ')
+        self.telefone = input('Qual seu telefone?')
+
+thiago = Pessoa
